26fa1711200cedf5bd48840c4340c2f337175d84
Martavias Cary <martavias.cary@outlook.com>
2022-05-16 01:23:04 -0400

    styling

---
+++ src/assets/components/Sidebar/index.js
@@ -0,0 +1,47 @@
+import {Link, NavLink} from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../images/logo-s.png'
+import LogoSubtitle from '../../images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar= () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo" />
+            <img className= "sub-logo" src={LogoSubtitle} alt="slobodan" />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4de4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4de4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4de4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target ="_blank" rel='noreferrer' href='https://www.linkedin.com/in/martaviascary/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BEcxwTLuQSDa%2BDDAUpUvzJQ%3D%3D'>
+                    <FontAwesomeIcon icon = {faLinkedin} color ="4d4d4e" />
+                </a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <a target ="_blank" rel='noreferrer' href='https://github.com/MarsProgrammingLab'>
+                    <FontAwesomeIcon icon = {faGithub} color ="4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
