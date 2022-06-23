acf8d5e0bdc770d28816fd35e900225d241665b8
Martavias Cary <martavias.cary@outlook.com>
2022-06-23 17:45:07 -0400

    fix routing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
    <>
    <Routes>
-     <Route path="/" element={<Layout /> }>
+     <Route path="/Martavias-Cary" element={<Layout /> }>
        <Route index element = {<Home />} />
        </ Route>
    </Routes>
